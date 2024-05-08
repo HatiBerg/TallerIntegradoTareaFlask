aa32e00c027684731255cc4b0b8aa24a89bb827a
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-05-08 19:33:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("")
+def index():
+    return render_template("index.html")
+
+# @app.route("/<string:slug>/")
+# def def1(slug):
+#     return render_template("template1.html", slug_title=slug)
+
+# @app.route("/<string:slug>/")
+# def def2(slug):
+#     return render_template("template2.html", slug_title=slug)
