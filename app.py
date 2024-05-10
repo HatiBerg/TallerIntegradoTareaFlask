cc6e64f302e4dd6aedd4da43f1e31bf1059229df
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-05-10 00:48:00 -0400

    final version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, redirect, render_template, url_for
 import json
 import random
 
@@ -7,24 +7,34 @@ app = Flask(__name__)
 with open('C:/Users/resid/GitHub/TallerIntegradoTareaFlask/static/vgsales.json', 'r') as j:
   data = json.load(j)
 
-def random_platform():
-  platform = set(game['platform'] for game in data)
-  rd_platform = random.choice(list(platform))
-  return rd_platform
+def get_all_platforms():
+    platforms = set(game['platform'] for game in data)
+    return list(platforms)
+platforms = get_all_platforms()
 
-def random_genre():
-  genres = set(game['genre'] for game in data)
-  rd_genre = random.choice(list(genres))
-  return rd_genre
+def get_all_genres():
+    genres = set(game['genre'] for game in data)
+    return list(genres)
+genres = get_all_genres()
 
 @app.route("/")
 def index():
   return render_template("index.html", data=data)
 
+@app.route("/platform/")
+def plataformas():
+    random_platform = random.choice(platforms)
+    return redirect(url_for('plataforma_randon', slug=random_platform))
+
 @app.route("/platform/<string:slug>/")
-def plataformas(slug):
-    return render_template("plataformas.html", data=data, random_platform=random_platform(), slug_platform=slug)
+def plataforma_randon(slug):
+    return render_template("plataformas.html", data=data, random_platform=slug)
+
+@app.route("/genre/")
+def generos():
+  random_genre = random.choice(genres)
+  return redirect(url_for('genero_randon', slug=random_genre))
 
 @app.route("/genre/<string:slug>/")
-def generos(slug):
-    return render_template("generos.html", data=data, random_genre=random_genre(), slug_genre=slug)
+def genero_randon(slug):
+    return render_template("generos.html", data=data, random_genre=slug)
